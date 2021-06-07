77f80868f27ef23f2cd070687efd008ace61595a
Ashwani <ashwani.k88@gmail.com>
2021-06-07 12:36:53 +0530

    php started

---
+++ info.php
@@ -0,0 +1,63 @@
+<?php
+$n = 123;
+
+echo "Hello $n";
+echo '<br>';
+echo 'Hello $n';
+
+echo '<br>';
+
+echo "Testing \"double\" qoutes";
+
+die; // exit;
+
+$a = 4;
+$b = 5;
+
+$a = '4';
+$b = '7';
+
+$c = $a + $b;
+
+// echo 'C = ';
+echo 'C = ' . $c;
+
+
+if($c >= 10){
+	echo 'C is greatest';
+}elseif($c <= 20){
+	
+}else{
+	
+}
+
+// echo '<table border="1" width="300" height="200">';
+// echo '<tr><td>2</td><td>2</td><td>2</td></tr>';
+// echo '<tr><td>2</td><td>2</td><td>2</td></tr>';
+// echo '<tr><td>2</td><td>2</td><td>2</td></tr>';
+// echo '</table>';
+
+// while()
+/*
+	
+*/
+
+// find greatest number out of 4
+// prime numbers
+// simple interest
+// electricity bill
+// marksheet
+ ?>
+<!doctype html>
+<html>
+<head>
+	<title>PHP start</title>
+</head>
+<body>
+<table width="300" height="200" border="1"><tbody>
+<tr><td>2</td><td>2</td><td>2</td></tr>
+<tr><td>2</td><td><?php echo $c; ?></td><td>2</td></tr>
+<tr><td>2</td><td>2</td><td>2</td></tr></tbody></table>
+</body>
+</html>
+ 
